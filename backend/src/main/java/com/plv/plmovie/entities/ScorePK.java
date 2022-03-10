50d85b59e5f7834d076cd153aee9ea6512773444
Pedro Veppo (Valtech.com) <pedro.veppo@valtech.com>
2022-03-10 15:36:54 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/plv/plmovie/entities/ScorePK.java
@@ -0,0 +1,50 @@
+package com.plv.plmovie.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Embeddable
+public class ScorePK implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+	
+	@ManyToOne
+	@JoinColumn(name = "movie_id")
+	private Movie movie;
+	
+	public ScorePK() {
+		
+	}
+
+	public ScorePK(User user, Movie movie) {
+		super();
+		this.user = user;
+		this.movie = movie;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Movie getMovie() {
+		return movie;
+	}
+
+	public void setMovie(Movie movie) {
+		this.movie = movie;
+	}
+}
