9059d9606788a1522b812b45e05f9271de42872c
Pedro Veppo (Valtech.com) <pedro.veppo@valtech.com>
2022-03-09 21:06:23 -0300

    Project created

---
+++ backend/src/main/java/com/plv/plmovie/PlmovieApplication.java
@@ -0,0 +1,13 @@
+package com.plv.plmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlmovieApplication.class, args);
+	}
+
+}
