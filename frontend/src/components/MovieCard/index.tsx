b7dbd9ed3aa2e5473b0c6f4985022846114aeb61
Pedro Veppo (Valtech.com) <pedro.veppo@valtech.com>
2022-03-12 15:07:43 -0300

    Show score

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -12,7 +12,7 @@ function MovieCard( { movie } : Props ) {
             <img className="plmovie-movie-card-image" src={movie.image} alt={movie.title} />
             <div className="plmovie-card-bottom-container">
                 <h3>{movie.title}</h3>
-                <MovieScore />
+                <MovieScore count={movie.count} score={movie.score} />
 
                 <Link to={`/form/${movie.id}`}>
                     <div className="btn btn-primary plmovie-btn">Avaliar</div>
--- frontend/src/components/MovieScore/index.tsx
+++ frontend/src/components/MovieScore/index.tsx
@@ -1,15 +1,16 @@
 import MovieStars from "components/MovieStars/Index";
 import './styles.css';
 
-function MovieScore() {
-
-    const score = 3.5;
-    const count = 13;
+type Props = {
+    score: number;
+    count: number;
+}
+function MovieScore( { score, count} : Props) {
 
     return (
         <div className="plmovie-score-container">
             <p className="plmovie-score-value">{score > 0 ? score.toFixed(1) : '-'}</p>
-            <MovieStars />
+            <MovieStars score={score}/>
             <p className="plmovie-score-count">{count} avaliações</p>
         </div>
     );
